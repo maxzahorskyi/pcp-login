80c8b831e4146f77d75990e3228016640f01a7a3
maxzahorskyi <maxim.zahorskyi.freeua@gmail.com>
2023-06-26 23:21:55 +0300

    added more pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PrivateRoute } from '../components/PrivateRoute/PrivateRoute';
+import ContactUs from '../pages/ContactUs';
+import ForgotPassword from '../pages/ForgotPassword';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import PrivacyPolicy from '../pages/PrivacyPolicy';
+import Terms from '../pages/Terms';
 
 function App() {
   return (
@@ -16,6 +20,10 @@ function App() {
           }
         />
         <Route path="login" element={<Login />} />
+        <Route path="forgot" element={<ForgotPassword />} />
+        <Route path="terms" element={<Terms />} />
+        <Route path="contact" element={<ContactUs />} />
+        <Route path="privacy" element={<PrivacyPolicy />} />
       </Routes>
     </BrowserRouter>
   );
