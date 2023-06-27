af3cb98d5b5c4df36fc06b0fd926d6be22eb4b76
maxzahorskyi <maxim.zahorskyi.freeua@gmail.com>
2023-06-27 16:31:39 +0300

    added click on logo

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -1,14 +1,20 @@
 import { Box, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { LogoIcon } from '../../assets/icons/logo.icon';
+import { getUser } from '../../store/slice/user.slice';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function PageTitle({ children }: Props) {
+  const user = useSelector(getUser);
   return (
     <Box>
-      <LogoIcon />
+      <NavLink to={user.token ? '/' : '/login'}>
+        <LogoIcon />
+      </NavLink>
       <Typography variant="h2" mt={4} mb={4.5} sx={{ fontSize: '56px', letterSpacing: '-0.5px' }}>
         {children}
       </Typography>
