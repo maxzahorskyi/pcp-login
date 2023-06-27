922cf856ba0efa23b3c1d71b24339153d3708e6e
maxzahorskyi <maxim.zahorskyi.freeua@gmail.com>
2023-06-27 16:28:22 +0300

    added notification call and show

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,25 @@
-import { Box } from '@mui/material';
-import { useDispatch } from 'react-redux';
+import CloseIcon from '@mui/icons-material/Close';
+import { Box, IconButton, Snackbar } from '@mui/material';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { Button } from '../app/app.styled';
 import PageTitle from '../components/PageTitle/PageTitle';
 import PageLayout from '../layout/page.layout';
-import { logoutUser } from '../store/slice/user.slice';
+import { useGetNotificationQuery } from '../store/api/notification.api';
+import { getUser, logoutUser } from '../store/slice/user.slice';
 
 export default function Home() {
   const distpatch = useDispatch();
   const navigate = useNavigate();
+
+  const user = useSelector(getUser);
+
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+  const { data: notification } = useGetNotificationQuery({ id: user.userId });
+
+  const handleClose = () => setIsOpen(false);
 
   const handleLogout = () => {
     distpatch(logoutUser());
@@ -26,6 +37,18 @@ export default function Home() {
         <Button variant="contained" onClick={handleLogout}>
           Logout
         </Button>
+        <Snackbar
+          sx={{ height: '100%' }}
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          open={isOpen}
+          onClose={handleClose}
+          message={`${notification?.title}! ${notification?.description}`}
+          action={
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          }
+        />
       </Box>
     </PageLayout>
   );
--- src/store/api/notification.api.ts
+++ src/store/api/notification.api.ts
@@ -3,13 +3,19 @@ import { globalApi } from './global.api';
 
 export const notificationApi = globalApi.injectEndpoints({
   endpoints: (build) => ({
-    getNotifications: build.query<INotification[], void>({
-      query: () => ({
-        url: `notifications`,
-        method: 'GET',
-      }),
+    getNotification: build.query<INotification, { id: number }>({
+      query: ({ id }) => {
+        const token = localStorage.getItem('token');
+        return {
+          url: `notifications/${id}`,
+          method: 'GET',
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        };
+      },
     }),
   }),
 });
 
-export const { useGetNotificationsQuery } = notificationApi;
+export const { useGetNotificationQuery } = notificationApi;
