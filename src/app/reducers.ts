069936a264b05ef73534709581092060d39bdd0d
maxzahorskyi <maxim.zahorskyi.freeua@gmail.com>
2023-06-27 15:31:40 +0300

    removed unneeded things

--- src/app/reducers.ts
+++ src/app/reducers.ts
@@ -1,10 +1,8 @@
 import { globalApi } from '../store/api/global.api';
-import notificationReducer from '../store/slice/notification.slice';
 import userReducer from '../store/slice/user.slice';
 
 const rootReducer = {
   user: userReducer,
-  notification: notificationReducer,
   [globalApi.reducerPath]: globalApi.reducer,
 };
 
